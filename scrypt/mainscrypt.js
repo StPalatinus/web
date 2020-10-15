3ed47902a1569d723f27ed0433a95bfb2d3ceb92
StPalatinus <PalaCisco@gmail.com>
2020-10-15 04:10:32 +0300

    modile slader beta 0.2

--- scrypt/mainscrypt.js
+++ scrypt/mainscrypt.js
@@ -1,17 +1,15 @@
   'use strict';
     const       showText = "Показать все";
     const       hideText = "Скрыть";
-    const       showHide = document.querySelector('.showhide');
+    const       showHide = document.querySelector('.button__showhide');
     const       brandsContainer = document.querySelector('.brands__container');
     
     showHide.onclick = function () {
         if (showHide.classList.contains('showhide__opened')) {
-            console.log(showHide.classList);
             showHide.textContent = showText;
             brandsContainer.classList.remove('brands__container--maximized');
             showHide.classList.remove('showhide__opened');
         } else {
-            console.log(showHide.classList);
             showHide.textContent = hideText;
             brandsContainer.classList.add('brands__container--maximized');
             showHide.classList.add('showhide__opened');
